0483587298d631b00d6e65459f108e03f4439060
Nishant Rishab <imreprisal@gmail.com>
2022-02-13 12:55:22 +0530

    Splash Activity and SplashScreen added

--- app/src/main/java/com/example/fidoo/API/Network.kt
+++ app/src/main/java/com/example/fidoo/API/Network.kt
@@ -1,11 +1,15 @@
 package com.example.fidoo.API
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Network {
+    val httploggingintercepter= HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     fun getRetrofit() = Retrofit.Builder()
         .baseUrl("https://gorest.co.in/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(OkHttpClient.Builder().addInterceptor(httploggingintercepter).build())
         .build()
 }
